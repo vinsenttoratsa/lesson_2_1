ca4b4ee8704d0c6f06a5698810d8d4b8185a7c57
vinsenttoratsa <vinsentotoratsa@gmail.com>
2025-09-24 12:17:56 +0300

    Update index.php
    добавил возврат, если первое число не число

--- index.php
+++ index.php
@@ -1,19 +1,30 @@
 <?php
-do {
+while (true) {
     echo "Введите число №1: ";
     $number1 = trim(fgets(STDIN));
-} while (!is_numeric($number1));
+    
+    if (is_numeric($number1)) {
+        break;
+    }
+    fwrite(STDERR, "Ошибка: введите число!\n");
+}
 
-do {
+while (true) {
     echo "Введите число №2: ";
     $number2 = trim(fgets(STDIN));
     
     if (!is_numeric($number2)) {
         fwrite(STDERR, "Ошибка: введите число!\n");
-    } elseif ((int)$number2 == 0) {
-        fwrite(STDERR, "Ошибка: делить на 0 нельзя!\n");
+        continue;
     }
-} while (!is_numeric($number2) || (int)$number2 == 0);
+    
+    if ((int)$number2 == 0) {
+        fwrite(STDERR, "Ошибка: делить на 0 нельзя!\n");
+        continue;
+    }
+    
+    break;
+}
 
 $number1 = (int)$number1;
 $number2 = (int)$number2;
